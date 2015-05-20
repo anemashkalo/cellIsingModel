c866d1c5be8d9c53e4fb562f11ace2fc482a82bc
Anastasiia <nmnemashkalo@gmail.com>
2015-05-20 13:52:42 -0500

    update, still in progress

--- ThreeStatePartitionFn_AN.m
+++ ThreeStatePartitionFn_AN.m
@@ -1,26 +1,48 @@
 % calculates the partition function and the associated probabilities (np) for the three-state model
 
 function [np]= ThreeStatePartitionFn_AN(N,B,J)
- 
-for n=0:N
-    for m=0:N 
-        %while ~(m == n)
-       k = N-(n+m);
-        C = nchoosek(N,n);
-        D = nchoosek(N-n,m);
-        S = B*(n-m);
-        d = (J)*(nchoosek(n,2)+nchoosek(m,2)+nchoosek(k,2)-m*n-n*k-m*k);
-        m
-        p(n+1) = C*D*exp(S)*exp(d); % non normalized probability
-        %P = P+p(n);
-        %  partition function
-       % end
-    end
-    n
-end
 
-P=sum(p);   %  partition function, need to modify since the summation was over two indices
-np=p/P;     %  normalized probability
+if N<3
+    [np]= PartitionFn_AN(N,B,J);
+end
+if N>=3
+    
+for n=0:N
+    
+    for m=0:N
+        k = N -(n+m);
+        while (n+m)<N
+        
+        C = nchoosek(N,n);
+        D = nchoosek((N-n),m);
+        S = B*(n-m);
+        
+        if n == 1
+           d = (J)*(nchoosek(m,2)+nchoosek(k,2)-m*n-n*k-m*k);
+        end
+        if m == 1
+           d = (J)*(nchoosek(n,2)+nchoosek(k,2)-m*n-n*k-m*k);
+        end
+                if k == 1
+                    d = (J)*(nchoosek(n,2)+nchoosek(m,2)-m*n-n*k-m*k);
+                end
+                       
+        d = (J)*(nchoosek(n,2)+nchoosek(m,2)+nchoosek(k,2)-m*n-n*k-m*k);  
+        end
+                        
+        p(n+1) = C*D*exp(S)*exp(d); % non normalized probability 
+        end
+        
+        end
+    end
+    
+   
+
+
+%P=sum(p);   %  partition function, need to modify since the summation was over two indices
+%np=p/P;     %  normalized probability
+
+np ;  % troubleshooting, calculate nonnomalised P
 
 % for n=1:N
 %   np(n)=p(n)/P ;
