98dd8a369febeaedb004e7429e248a22bcee35ce
Anastasiia <nmnemashkalo@gmail.com>
2015-05-27 14:41:35 -0500

    smallUpdatesAN

--- Fractions_AN.m
+++ Fractions_AN.m
@@ -3,27 +3,29 @@ function [fr] = Fractions_AN(N,B,J)
 if N == 1
 np= PartitionFn_AN(N,B,J);
 fr=0;
- %for k=0:N
+ 
      for n=0:N
     expect(n+1) = ((n)/N)*(np(n+1)); % 
-        %fr(n+1) = sum(expect);
+       
     end
 fr = sum(expect);
 else
 %if model  == 3 || model >3
        np = ThreeStatePartitionFn_AN(N,B,J);
+       pp = sum(np,2);
  fr=0;
       
 for n=0:N
-    for m=0:(N-n)
-    expect(n+1,m+1)= ((n)/N)*np(n+1,m+1); % 
+    %for m=0:(N-n)
+        
+        expect(n+1) = ((n)/N)*pp(n+1);
+        %expect(n+1,m+1)= ((n)/N)*np(n+1,m+1); 
     
-    %fr(n+1) = sum(expect);
-    end
+    %end
 end
 
 fr = sum(expect);
-fr = sum(fr);
+% fr = sum(fr);
  %end
 %end 
  
