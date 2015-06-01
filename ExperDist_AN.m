883bb443d9c87dcdecf545236eea7854967e2215
Anastasiia <nmnemashkalo@gmail.com>
2015-06-01 17:31:14 -0500

    nothing

--- ExperDist_AN.m
+++ ExperDist_AN.m
@@ -1,50 +1,46 @@
 
-function [binN, totalcoloniesN, np] = ExperDist_AN(Nplot,nms,thresh,nms2,index,value,B,J,N)
+function [binN, totalcoloniesN, np] = ExperDist_AN(nms,thresh,nms2,index,value,B,J,N)
 
 %-------------------------
 
 [np]= PartitionFn_AN(N,B,J);
-
-  vect=1:N+1;
-  plot(vect,np,'m--*'); legend('Calculated values');hold on % plot normalized probabilities
-  ylim([0 1.1]);
-  xlim([0 N+1]);
-  hold on
+% [np] = ThreeStatePartitionFuction_AN(N,B,J)
+vect=1:N+1;
+plot(vect,np,'m--*'); legend('Calculated values');hold on % plot normalized probabilities
+ylim([0 1.1]);
+xlim([0 N+1]);
+hold on
 %------------------------
-  
-%   %function [ratios, ratios2, totalcells] = ColAnalysisNoutfiles(Nplot,nms,thresh,nms2,index,value) 
-for k=1:Nplot
-     % colonies{k}=[];
-     filename = ['.' filesep  nms{k} '.mat'];
-     load(filename);
-%     
-% %     load(filename,'plate1');
-% %     disp([filename,'plate1']);
-% %     colonies{k} = plate1.colonies;
-%     
-%    % if ~exist('plate1')
-%         
-%     load(filename);
-     disp([filename]);
-    [colonies{k}, peaks]=peaksToColoniesSC(filename);
-     end
-%     
-% end
 
-% fraction of cells that are sox17 positive vs nuber of cells in the colony
+%   %function [ratios, ratios2, totalcells] = ColAnalysisNoutfiles(Nplot,nms,thresh,nms2,index,value)
+for k=1:size(nms,2)
+    
+    filename{k} = ['.' filesep  nms{k} '.mat'];
+    
+    load(filename{k},'peaks','dims','plate1');
+    colonies{k} = plate1.colonies;
+    if ~exist('plate1','var')
+        [colonies{k}, ~]=peaksToColonies(filename);
+    end
+    M(k) = max([colonies{k}.ncells]);
+    
+    % [toplot,peaks] = GetSeparateQuadrantImgNumbersAN(nms2,peaks,dims,midcoord,fincoord);
+end
+M = max(M);
 
-for k=1:Nplot
+% obtaine experimental distributions ( within the colony size N)
+
+for k=1:size(nms,2)
     
     totalcolonies = zeros(N+1,1);
     binN = zeros(N+1,1);
-      
     totalcells=zeros(N+1,1);
     col = colonies{k};
     % nc=0;
     for ii=1:length(col)
         nc = col(ii).ncells;
         if ~isempty(col(ii).ncells) && nc == N;
-            %nc = col(ii).ncells;
+            
             totalcolonies(nc)=totalcolonies(nc)+1;
             % totalcells(nc)=totalcells(nc)+nc;
             X = col(ii).data(:,value)./col(ii).data(:,5)>thresh;
@@ -61,42 +57,17 @@ for k=1:Nplot
     % need to normalize to the sum of colonies with N cells ( the
     % overall total number of N-cell colonies)
     
-    totalcoloniesN = nonzeros(totalcolonies); 
+    totalcoloniesN = nonzeros(totalcolonies);
     allcells = sum(totalcells);
-   % binN = nonzeros(binN); 
+    
     binN = binN/totalcoloniesN;
-   % binN = sort(binN,'descend');  % order of the plotted values  
+    
     plot(binN,'b*');
     xlim([0 N+1]);
     
     xlabel('Number of cells in the colony');
-    ylabel(['Probability       ',(index)]);
+    ylabel(['Probability',(index)]);
     
-%     ratios = genepositive./totalcells;
-%     ratios2 = geneposcolonies./totalcolonies;
-     
-%------------------------------------------------------------------------------------------    
-    %subplot(2,2,k), plot(sox17colonies,'b*');legend(nms2{k});hold on subplot(2,2,k)
-%     figure(6);subplot(2,2,k), plot(ratios,'b*'); legend(nms2{k});
-%     xlabel('Number of cells in the colony');
-%     ylabel(['FractionOf',(index),'PositiveCells']);
-%     title ([thresh]);
-%     xlim([0 10]);
-%     ylim([0 1]);
-%     
-%     figure(1);  subplot(2,2,k), plot(ratios2,'b*'); legend(nms2{k});
-%     xlabel('Number of cells in the colony');
-%     ylabel(['FractionOf',(index),'PositiveColonies']);
-%     title ([thresh]);
-%     xlim([0 10]);
-%     ylim([0 1]);
-%     
-%     figure(4);  subplot(2,2,k), plot(totalcells,'b*'); legend(nms2{k});
-%     xlabel('Number of cells in the colony');
-%     ylabel('Total cells');
-%     
-%     title ([thresh]);
-%     xlim([0 10]);
     
 end
 end
